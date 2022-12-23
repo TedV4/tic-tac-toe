7660af9866ef973482ba069f9c86cc487318d573
TedV4 <tedverdonkschot@outlook.com>
2022-12-23 16:31:34 -0700

    Add function for checking win condition patterns, finish game logic for determing winner, determining draw, and restarting the game

--- app.js
+++ app.js
@@ -30,11 +30,10 @@ const gameLogic = (() => {
       cell.addEventListener("click", cellClicked)
     );
     displayController.restartBtn.addEventListener("click", restartGame);
-    console.log(displayController.cells)
     displayController.gameInfo.textContent = `${currentPlayer}'s turn`;
     running = true;
   }
-  
+
   //If a cell is empty and the game is running, continue to mark the clicked cell
   function cellClicked() {
     const cellIndex = this.getAttribute("cellIndex");
@@ -53,17 +52,47 @@ const gameLogic = (() => {
   }
 
   function changePlayer() {
-    currentPlayer = (currentPlayer == 'X' ? 'O' : 'X');
+    currentPlayer = currentPlayer == "X" ? "O" : "X";
     displayController.gameInfo.textContent = `${currentPlayer}'s turn`;
   }
 
   function checkWinner() {
     let roundComplete = false;
+    //If all elements of a win condition are equal, and are not empty, complete the round
+    for (let i = 0; i < winConditions.length; i++) {
+      const condition = winConditions[i];
+      const cellA = coordinates[condition[0]];
+      const cellB = coordinates[condition[1]];
+      const cellC = coordinates[condition[2]];
 
-    
+      if (cellA === "" || cellB === "" || cellC === "") {
+        continue;
+      }
+
+      if (cellA === cellB && cellB === cellC) {
+        roundComplete = true;
+        break;
+      }
+    }
+
+    if (roundComplete === true) {
+      displayController.gameInfo.textContent = `${currentPlayer} wins!`;
+      running = false;
+    } else if (!coordinates.includes("")) {
+      displayController.gameInfo.textContent = "Draw!";
+      running = false;
+    } else {
+      changePlayer();
+    }
   }
 
-  function restartGame() {}
+  function restartGame() {
+    currentPlayer = "X";
+    coordinates = ["", "", "", "", "", "", "", "", ""];
+    displayController.cells.forEach((cell) => (cell.textContent = ""));
+    displayController.gameInfo.textContent = `${currentPlayer}'s turn`;
+    running = true;
+  }
 
   function playRound() {
     initGame();
