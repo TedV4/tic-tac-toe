1bf2797900af5258f165c2a90b96198450e5ab71
TedV4 <tedverdonkschot@outlook.com>
2022-12-22 20:38:51 -0700

    Add winconditions, a function that checks if winconditions are met, and log results to console

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-const gameBoard = (() => {
-  const grid = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-  return {
-    grid,
-  };
-})();
-
 const Player = (marker) => {
   return { marker };
 };
@@ -16,8 +8,35 @@ const player2 = Player("O");
 const Game = (() => {
   const gameBoard = document.querySelector(".grid-container");
 
+  let square = { board: [1, 2, 3, 4, 5, 6, 7, 8, 9] };
   let turn = 0;
   let currentPlayer = player1;
+  let winner = '';
+  let winConditions = [
+    [square.board[0], square.board[1], square.board[2]],
+    [square.board[3], square.board[4], square.board[5]],
+    [square.board[6], square.board[7], square.board[8]],
+    [square.board[0], square.board[3], square.board[6]],
+    [square.board[1], square.board[4], square.board[7]],
+    [square.board[2], square.board[5], square.board[8]],
+    [square.board[0], square.board[4], square.board[8]],
+    [square.board[6], square.board[4], square.board[2]],
+  ];
+
+
+  function allEqual(arr) {
+    if (arr.every((val) => val === arr[0]) === true) {
+      console.log("win");
+      winner = arr[0];
+    }
+  }
+
+  function checkForWin() {
+    for (let i = 0; i < winConditions.length; i++) {
+      console.log(winConditions[i]);
+      allEqual(winConditions[i]);
+    }
+  }
 
   function getCurrentPlayer() {
     if (turn == 0 || turn % 2 == 0) {
@@ -31,15 +50,32 @@ const Game = (() => {
     for (let i = 0; i < 9; i++) {
       let cell = document.createElement("div");
       cell.classList.add("cell");
-      //Create a game function to sub here
-      cell.addEventListener('click', function() {
-        cell.textContent = currentPlayer.marker;
-        turn++;
-        getCurrentPlayer();
-      })
+      cell.id = `cell${i}`;
+      cell.addEventListener("click", function (e) {
+        let cellNumber = parseInt(e.target.id[4]);
+        console.log(cellNumber);
+        if (cell.textContent === "") {
+          cell.textContent = currentPlayer.marker;
+          square.board[cellNumber] = currentPlayer.marker;
+          turn++;
+          getCurrentPlayer();
+          winConditions = [
+            [square.board[0], square.board[1], square.board[2]],
+            [square.board[3], square.board[4], square.board[5]],
+            [square.board[6], square.board[7], square.board[8]],
+            [square.board[0], square.board[3], square.board[6]],
+            [square.board[1], square.board[4], square.board[7]],
+            [square.board[2], square.board[5], square.board[8]],
+            [square.board[0], square.board[4], square.board[8]],
+            [square.board[6], square.board[4], square.board[2]],
+          ];
+          checkForWin();
+        }
+      });
       gameBoard.appendChild(cell);
     }
   };
+
   return { gameBoard, startGame };
 })();
 
